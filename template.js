9cb1fa0e0d564622e6542720f4c209c3796885f9
cocode <github.com@worldware.com>
2015-10-03 19:06:23 -0700

    Now finding and replacing all keys.

--- template.js
+++ template.js
@@ -3,20 +3,26 @@
 var fs = require('fs')
 var controlFile
 var key = "@@"
+var output = ""
 
 for (var i=2; i < process.argv.length; i++) {
     var src  = process.argv[i]
-    console.log(src)
+    //console.log(src)
     var contents = fs.readFileSync(src, 'utf8')
     var nn = contents.search(key)
     while (nn !== -1) {
-        console.log("nn = " + nn)
-        var part = contents.substring(nn+key.length)
+        var keyStart = nn
+        output = output.concat(contents.substring(0,keyStart))
+        var part = contents.substring(keyStart + key.length)
         var mm = part.search(/\W/)
-        console.log("mm = " + mm)
         var ref = part.substring(0,mm)
-        console.log(ref)
+        output = output.concat(ref.toUpperCase())
+        //console.log(ref)
         contents = part.substring(mm)
         nn = contents.search(key)
+        if (nn === -1) {
+            output = output.concat(part)
+        }
     }
 }
+console.log(output)
