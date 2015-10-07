e27d6fc0363102fcedd2db3bbddc4862f5678a96
cocode <github.com@worldware.com>
2015-10-06 23:14:14 -0700

    Allow individual pages to turn off the jumbotron. Fix link to .mp3 in congress.inc.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ node template.js 'sub=Home Page'                contents=@index.inc template.src
 node template.js 'sub=Dance Classes'            contents=@classes.inc template.src > classes.html
 node template.js 'sub=What to Wear'             contents=@costumes.inc template.src > costumes.html
 node template.js 'sub=Location and Map'         contents=@location.inc template.src > location.html
-node template.js 'sub=Congress of Vienna Waltz' contents=@congress.inc template.src > congress.html
+node template.js 'sub=Congress of Vienna Waltz' contents=@congress.inc jumbotron="" template.src > congress.html
 node template.js 'sub=Next Ball'                contents=@upcomingevents.inc template.src > upcomingevents.html
